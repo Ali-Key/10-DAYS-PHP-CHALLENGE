bd4b113b19f60e7d3b53a5c52848c6b541a61cba
Ali Omar Abdi <Mafiobio700@gmail.com>
2023-08-20 21:31:19 +0300

    event of jquery

--- Day7/index.php
+++ Day7/index.php
@@ -24,13 +24,15 @@
 
 
 
-    <h2 class="header_one">
-        Hellow World
-    </h2>
+  <h2 class="header_one">
+    Hellow World
+  </h2>
 
-    <h2 class="header_two">
-        Hey, Iam Ali Omar
-    </h2>
+  <h2 class="header_two">
+    Hey, Iam Ali Omar
+  </h2>
+
+  <button> CHANGE BODY COLOR</button>
 
 
 
@@ -41,8 +43,9 @@
 
   <!-- jquery cdn -->
 
-  <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
-   
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+    integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+
   <!-- JavaScript file -->
 
   <script src="main.js"></script>
@@ -50,11 +53,11 @@
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
     integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
+    </script>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
+    </script>
 </body>
 
 </html>
--- Day7/main.js
+++ Day7/main.js
@@ -6,4 +6,30 @@
 // $("tagname") - to select all the elements with tag name  - returns  tagname elements
 // $(".classname") - to select all the elements with class name  - returns  classname elements
 // $("#idname") - to select all the elements with id name  - returns  idname elements
+
+
+
+
+
 $(".header_one").html("Hello Somalia");
+$(".header_two").css("color", "red");
+
+
+// event of jquery 
+
+$("button").click(function(){
+    $("body").css("background" , "red" );
+    $("body").css("margin" , "20px" );
+    $("body").css("fontFamily" , "Segoe UI  Regular Expressions");
+    $("body").css("text-align" , "center");
+  
+    $(".header_one ").css("color", "green");
+    $(".header_two ").css("color", "green");
+  
+})
+
+$("body").css("margin" , "20px" );
+
+
+$("button").css("fontSize", " 12px");
+$("button").css("padding" , "10px");
