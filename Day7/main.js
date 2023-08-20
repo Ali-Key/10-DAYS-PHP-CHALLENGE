459a1bbec669719c2dbe76c1591cc4b74963b428
Ali Omar Abdi <Mafiobio700@gmail.com>
2023-08-20 21:01:09 +0300

    Basic Jquery

---
+++ Day7/main.js
@@ -0,0 +1,9 @@
+
+// . is used to class name
+// # is used to id name
+// tag name is used html tag element
+
+// $("tagname") - to select all the elements with tag name  - returns  tagname elements
+// $(".classname") - to select all the elements with class name  - returns  classname elements
+// $("#idname") - to select all the elements with id name  - returns  idname elements
+$(".header_one").html("Hello Somalia");
